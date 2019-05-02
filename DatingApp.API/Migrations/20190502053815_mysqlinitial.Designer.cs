10b205eadf52b2d0e9f0e15c6bd47fe316440b20
Samuel Omamalin <samuelomamalin@gmail.com>
2019-05-02 03:18:31 -0400

    switch to mysql

--- DatingApp.API/Migrations/20190502053815_mysqlinitial.Designer.cs
+++ DatingApp.API/Migrations/20190502053815_mysqlinitial.Designer.cs
@@ -9,14 +9,15 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20190428131636_AddedMessageEntity")]
-    partial class AddedMessageEntity
+    [Migration("20190502053815_mysqlinitial")]
+    partial class mysqlinitial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+                .HasAnnotation("ProductVersion", "2.2.0-rtm-35687")
+                .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("DatingApp.API.Models.Like", b =>
                 {
